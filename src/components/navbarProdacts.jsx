e37aac7fa0cdaba8f6cc816b4343e387f1fee74d
shirasadon <shirasa199@gmail.com>
2022-11-12 21:32:13 +0200

    adding style to navbarproducts component

--- src/components/navbarProdacts.jsx
+++ src/components/navbarProdacts.jsx
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
-import style from "../style/navbarproduct.css"
+import   "../style/navbarproduct.css"
 function NavbarProduct() {
   return (
     <>
-
-      <div className={style.container} class="container" id="navbarSupportedContent" >
-        <div  className="babycarriges">
+<div>
+      <div class="row"  >
+        <div className="stylediv" class=" stylediv col-sm">
             <Link to="/products/babycarriges" class="navbar-brand">
               <img
                 style={{
                   height: "50px",
                   width: "50px",
-                  backgroundColor: "#ffe4e1",
+                  backgroundColor: "#f08080",
                
                 }}
                 src="https://cdn-icons-png.flaticon.com/512/1014/1014615.png"
@@ -20,13 +20,13 @@ function NavbarProduct() {
             </Link>
             </div>
         
-          <div className="clothingandfootweare">
+          <div className="stylediv" class=" stylediv col-sm">
             <Link to="/products/clothingandfootweare" class="navbar-brand">
               <img
                 style={{
                   height: "50px",
                   width: "50px",
-                  backgroundColor: "#ffe4e1",
+                  backgroundColor: "#f08080",
                 }}
                 src="https://cdn-icons-png.flaticon.com/512/4464/4464080.png"
               ></img>
@@ -34,45 +34,46 @@ function NavbarProduct() {
             </Link>
             </div>
       
-         <div className="furniture" >
+         <div className="stylediv" class=" stylediv col-sm" >
             <Link to="/products/furniture" class="navbar-brand">
               <img
                 style={{
                   height: "50px",
                   width: "50px",
-                  backgroundColor: "#ffe4e1",
+                  backgroundColor: "#f08080",
                 }}
                 src="https://cdn-icons-png.flaticon.com/512/6992/6992633.png"
               ></img>
               furniture
             </Link>
             </div>
-        <div className="chairsafty">
+        <div className="stylediv" class=" stylediv col-sm">
             <Link to="/products/chairsafty" class="navbar-brand">
               <img
                 style={{
                   height: "50px",
                   width: "50px",
-                  backgroundColor: "#ffe4e1",
+                  backgroundColor: "#f08080",
                 }}
                 src="https://cdn-icons-png.flaticon.com/512/3262/3262527.png"
               ></img>
               Safety Chair
             </Link>
             </div>
-        <div className="toys">
+        <div className="stylediv" class=" stylediv col-sm">
             <Link to="/products/toys" class="navbar-brand">
               <img
                 style={{
                   height: "50px",
                   width: "50px",
-                  backgroundColor: "#ffe4e1",
+                  backgroundColor: "#f08080",
                 }}
                 src="https://cdn-icons-png.flaticon.com/512/3262/3262527.png"
               ></img>
               toys
             </Link>
             </div>
+        </div>
         </div>
     </>
   );
